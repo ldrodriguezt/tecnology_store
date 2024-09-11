f797496d23f647ce1ae2e2d1800aea6ffc0e79fb
ldrodriguezt <ldrodriguezt70135@umanizales.edu.co>
2024-09-11 18:28:27 -0500

    Eliminación de comentarios

--- Pages/search_computers.py
+++ Pages/search_computers.py
@@ -4,23 +4,23 @@ from computer_db_helper import get_computer_info, get_all_brands
 
 st.title("Consultar información de los equipos de cómputo por marca")
 
-# Obtener la lista de todas las marcas desde la base de datos
+
 brands = get_all_brands()
 
-# Si hay marcas disponibles, mostrar el dropdown
+
 if brands:
     st.header("Buscar equipos de cómputo por marca:")
     marca_seleccionada = st.selectbox("Seleccione una marca", options=brands)
     
-    # Botón para realizar la consulta
+    
     if st.button("Consultar"):
         if marca_seleccionada:
-            # Realizar la consulta a la base de datos con la marca seleccionada
+            
             results = get_computer_info(marca=marca_seleccionada)
 
             if results:
                 st.write(f"Se encontraron {len(results)} computadores de la marca '{marca_seleccionada}':")
-                # Mostrar los resultados en una tabla
+                
                 df = pd.DataFrame(results, columns=['Serial', 'Marca', 'Modelo', 'Procesador', 'Memoria RAM', 'Almacenamiento', 'Tipo'])
                 st.dataframe(df)
             else:
--- Pages/upload_computers.py
+++ Pages/upload_computers.py
@@ -5,7 +5,7 @@ from computer_db_helper import insert_computer_in_bulk, insert_computer_manually
 st.title("Agregar información de los equipos de cómputo")
 
 def extract_computers_from_excel(excel_file1, excel_file2):
-    """Extracts computer information from the provided Excel files and inserts into the database."""
+    
     try:
         df1 = pd.read_excel(excel_file1)
         df2 = pd.read_excel(excel_file2)
@@ -13,23 +13,23 @@ def extract_computers_from_excel(excel_file1, excel_file2):
         st.write(f"Error en la lectura del archivo en excel: {e}")
         return []
 
-    # Seleccionar columnas necesarias
+    
     df1 = df1[['serial', 'marca', 'modelo']]
     df2 = df2[['procesador', 'memoria_ram', 'almacenamiento', 'tipo']]
 
-    # Concatenar ambos DataFrames por columnas
-    merged_df = pd.concat([df1, df2], axis=1)
+    
+    concat_df = pd.concat([df1, df2], axis=1)
 
-    # Insertar en la base de datos
-    insert_computer_in_bulk(merged_df, table_name='computer')
+    
+    insert_computer_in_bulk(concat_df, table_name='computer')
 
-    st.write(merged_df)
+    st.write(concat_df)
 
     st.success("¡La información se ha registrado exitosamente!")
     
 
 def form_manual():
-    """Form to manually add a single computer to the database."""
+    
     st.subheader("Agregar información de equipo de cómputo")
 
     # Crear campos de entrada en el formulario
@@ -41,7 +41,7 @@ def form_manual():
     almacenamiento = st.text_input("Almacenamiento")
     tipo = st.text_input("Tipo")
 
-    # Botón para enviar datos
+    
     if st.button("Agregar datos"):
         if serial and marca and modelo and procesador and memoria_ram and almacenamiento and tipo:
             # Llamar la función para insertar en la base de datos
@@ -50,15 +50,15 @@ def form_manual():
         else:
             st.error("Por favor complete todos los campos.")
 
-# Mostrar el formulario manual
+
 form_manual()
 
-# Subir los archivos de Excel
+
 st.subheader("Subir lista de información de equipos de cómputo")
 uploaded_file1 = st.file_uploader("Por favor agregue el archivo que contiene los datos de los equipos:", type=["xls", "xlsx"])
 uploaded_file2 = st.file_uploader("Por favor agregue el archivo con las especificaciones de los equipos:", type=["xls", "xlsx"])
 
-# Botón para procesar la carga y mostrar los valores
+
 if st.button("Agregar datos de archivos"):
     if uploaded_file1 is not None and uploaded_file2 is not None:
         extract_computers_from_excel(uploaded_file1, uploaded_file2)
