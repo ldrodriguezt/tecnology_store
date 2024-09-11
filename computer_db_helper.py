3cce0b26585ff6df3d4d6521816fd3026783c4b3
ldrodriguezt <ldrodriguezt70135@umanizales.edu.co>
2024-09-10 22:27:13 -0500

    Formulario de registro manual de equipos

--- computer_db_helper.py
+++ computer_db_helper.py
@@ -5,11 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def clean_dataframe(df):
-    """Limpia el DataFrame de valores NaN en nombres de columnas y datos."""
-    df.columns = df.columns.fillna('')
-    df = df.fillna('')
-    return df
 
 def insert_computer_in_bulk(merged_df, table_name='computer'):
     connection = None
@@ -30,9 +25,6 @@ def insert_computer_in_bulk(merged_df, table_name='computer'):
 
         if connection.is_connected():
             cursor = connection.cursor()
-
-            # Limpia el DataFrame
-            merged_df = clean_dataframe(merged_df)
 
             # Verifica que el DataFrame tenga las columnas necesarias
             expected_columns = {'serial', 'marca', 'modelo', 'procesador', 'memoria_ram', 'almacenamiento', 'tipo'}
@@ -69,3 +61,47 @@ def insert_computer_in_bulk(merged_df, table_name='computer'):
         if connection is not None and connection.is_connected():
             connection.close()
 
+def insert_computer_manually(serial, marca, modelo, procesador, memoria_ram, almacenamiento, tipo, table_name='computer'):
+    connection = None
+    cursor = None
+
+    try:
+        if not table_name.isidentifier():
+            raise ValueError("Invalid table name.")
+
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            # Prepare the insert query for a single record
+            insert_query = f"""
+            INSERT INTO {table_name} (serial, marca, modelo, procesador, memoria_ram, almacenamiento, tipo)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """
+
+            # Execute the insert query with the provided data
+            cursor.execute(insert_query, (serial, marca, modelo, procesador, memoria_ram, almacenamiento, tipo))
+            
+            # Commit the transaction
+            connection.commit()
+
+            print(f"1 row inserted successfully.")
+
+    except Error as e:
+        print(f"Error: {e}")
+        if connection:
+            connection.rollback()
+    except ValueError as ve:
+        print(f"ValueError: {ve}")
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if connection is not None and connection.is_connected():
+            connection.close()
